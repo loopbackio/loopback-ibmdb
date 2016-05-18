1de435284143616c9649c41feda7a6023e24473a
Quentin Presley <qpresley@ca.ibm.com>
2016-05-18 09:52:14 -0700

    Update tests

---
+++ test/ibmdb.config.test.js
@@ -0,0 +1,22 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+/* eslint-env node, mocha */
+process.env.NODE_ENV = 'test';
+require('./init.js');
+var assert = require('assert');
+
+var config;
+
+before(function() {
+  config = global.config;
+});
+
+describe('testConfiguration', function() {
+  it('test config exists', function(done) {
+    assert(config);
+    done();
+  });
+});
