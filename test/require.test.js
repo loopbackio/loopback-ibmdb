8ab047b66aa225379618e2afbfe10663c9cb2bd5
Ryan Graham <r.m.graham@gmail.com>
2016-09-27 18:31:21 -0700

    test: include test that loads module
    The existing tests don't actually require() the module itself, so the
    native addon portion doesn't even get loaded.

---
+++ test/require.test.js
@@ -0,0 +1,12 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-ibmdb
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+/* eslint-env node, mocha */
+
+describe('module basics', function() {
+  it('can be required', function() {
+    require('../');
+  });
+});
