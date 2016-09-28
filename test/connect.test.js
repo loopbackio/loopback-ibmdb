1c93e3a0c6e38059064df9a299bfbdb6f538a649
Ryan Graham <r.m.graham@gmail.com>
2016-09-27 18:43:30 -0700

    test: add simple connection test

---
+++ test/connect.test.js
@@ -0,0 +1,35 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-ibmdb
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+/* eslint-env node, mocha */
+var EventEmitter = require('events').EventEmitter;
+var IBMDB = require('../').IBMDB;
+require('./init.js');
+
+describe('basic connector', function() {
+  var ds = new EventEmitter;
+  var connection = null;
+
+  it('can be constructed', function(done) {
+    ds.connector = new IBMDB('db2', global.config);
+    ds.connector.dataSource = ds;
+    done();
+  });
+
+  it('can connect', function(done) {
+    ds.connector.connect(function(err, conn) {
+      connection = conn;
+      done(err);
+    });
+  });
+
+  it('can ping', function(done) {
+    ds.connector.ping(done);
+  });
+
+  it('can disconnect', function(done) {
+    connection.close(done);
+  });
+});
