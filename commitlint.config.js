a108bb4cd1b4ea95a306e9dbe2bebd21938b12ca
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2024-11-01 21:29:19 +0800

    ci: enable ci pipelines
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright 2024 LoopBack contributors
+// SPDX-License-Identifier: MIT
+'use strict';
+
+const isCI = process.env.CI;
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [0, 'always'],
+    // Only enforce the rule if CI flag is not set. This is useful for release
+    // commits to skip DCO
+    'signed-off-by': [isCI ? 0 : 2, 'always', 'Signed-off-by:'],
+  },
+};
