eb07c182296d28e624b8b221818b135113e92f76
dhmlau <dhmlau@ca.ibm.com>
2025-07-16 15:59:03 -0400

    fix: test
    Signed-off-by: dhmlau <dhmlau@ca.ibm.com>

--- test/init.js
+++ test/init.js
@@ -8,8 +8,8 @@
 const DataSource = require('loopback-datasource-juggler').DataSource;
 
 const config = {
-  username: process.env.DB2_USERNAME || 'test',
-  password: process.env.DB2_PASSWORD || 'testpassword',
+  username: process.env.DB2_USERNAME || 'db2inst1',
+  password: process.env.DB2_PASSWORD || 'password',
   hostname: process.env.DB2_HOSTNAME || 'localhost',
   port: process.env.DB2_PORTNUM || 60000,
   database: process.env.DB2_DATABASE || 'testdb',
